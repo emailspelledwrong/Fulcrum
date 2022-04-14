f6cbddb8e5cb4e2009ad0ee428fc05c16e7288d7
Email Spelled Wrong <74515142+emailspelledwrong@users.noreply.github.com>
2022-04-14 08:53:30 -0600

    libInv - externalize utilities for the inv command
    I moved the isNumeric() function to the libInv object within the external libInv script file

---
+++ libInv.js
@@ -0,0 +1,8 @@
+var libInv = libInv || {};
+libInv.utils = {
+	isNumeric : function(str) {
+	if (typeof str != "string") return false; // we only process strings!  
+	return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+		   !isNaN(parseFloat(str)); // ...and ensure strings of whitespace fail
+	}
+}
