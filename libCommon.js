c30301ae2f14bd0901c584654462a0f7925668b1
Email Spelled Wrong <74515142+emailspelledwrong@users.noreply.github.com>
2022-04-16 13:14:09 -0600

    Segregating javascript
    I moved more function's, var's, const's to libINV.js and livCommon.js

---
+++ libCommon.js
@@ -0,0 +1,26 @@
+const _common_text = [
+	'Do not use commas, currency/percent signs, or spaces.', //0
+	'<br><span class="invalid-value">' //1
+]
+
+const _error_dictionary = [
+	'Error[000]: Specified less-than range is not valid. Use &lt;+numbers only (eg. &lt;123.67).',
+	'Error[001]: Specified greater-than range is not valid. Use &gt;+numbers only (eg. &gt;123.67).',
+	'Error[002]: &lt; or &gt; must be followed by a number.',
+	'Error[003]: &lt; or &gt; range may each only be used once in a command.',
+	'Error[004]: Query not found.'
+]
+
+var _errors = []; //array of error messages
+
+function escapeHtml(unsafe)
+{
+	unsafe = unsafe.valueOf();
+	return unsafe
+		 .replace(/&/g, "&amp;")
+		 .replace(/</g, "&lt;")
+		 .replace(/>/g, "&gt;")
+		 .replace(/"/g, "&quot;")
+		 .replace(/'/g, "&#039;");
+}
+
--- libInv.js
+++ libInv.js
@@ -1,8 +1,67 @@
-var libInv = libInv || {};
-libInv.utils = {
-	isNumeric : function(str) {
+var vscrap = [false,false,false,false,false,false]; //void,submitted,created,rejected,approved,paid
+
+const invoice_table_page = $('<table id="invoices">'+
+	'<tr><th>Invoice#</th><th>Customer</th><th>Status</th><th>Updated</th><th>Submitted</th><th>Paid</th><th class="currency">Value</th></tr>'+
+	'<tr><td>4549816</td><td>Tesla</td><td>created</td><td>2022-03-08 02:23:14PM</td><td></td><td></td><td class="currency">$14,779.81</td></tr>'+
+	'<tr><td>3178349</td><td>Amazon</td><td>submitted</td><td>2022-02-21 11:05:17AM</td><td>2022-03-02</td><td></td><td class="currency">$12,349,228.99</td></tr>'+
+	'<tr><td>4329817</td><td>Alphabet</td><td>paid</td><td>2022-02-14 08:21:54AM</td><td>2022-02-15</td><td>2022-03-29</td><td class="currency">$293,766.12</td></tr>'+
+	'<tr><td>0039141</td><td>Oracle</td><td>rejected</td><td>2022-03-08 04:29:13PM</td><td>2022-03-11</td><td></td><td class="currency">$4,723.05</td></tr>'+
+	'</table>'+
+	'\n<span>Page 1 of 999</span>'); //a fake set of invoice data
+	
+const inv_help = 'This is the help for the INV command.';
+	
+var ranges = [NaN,NaN]; //[less-than value, greater-than value]
+
+function isNumeric(str) {
 	if (typeof str != "string") return false; // we only process strings!  
 	return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
 		   !isNaN(parseFloat(str)); // ...and ensure strings of whitespace fail
+}
+
+function process_vscrap(val, i) {
+	const vscrap_args = ['void','submitted','created','rejected','approved','paid'];
+	if (vscrap_args.indexOf(val) != -1) {
+		vscrap[vscrap_args.indexOf(val)] = true;
+	}
+}	
+
+function process_queries(val, i) {
+	if (val.startsWith('@')) {
+		//load the named query
+		_errors.push($('<span>'+_error_dictionary[4]+'</span>'+_common_text[1]+escapeHtml(val)+'</span>'));
+	}
+}
+
+function process_ranges(val, i) {
+	if ((val.startsWith('>') || val.startsWith('<')) && val.length == 1) {
+		_errors.push($('<span>'+_error_dictionary[2]+'</span>'+_common_text[1]+escapeHtml(val)+'</span>'));
+		return;
+	}
+	
+	if (val.startsWith('<')) {
+		if (!isNaN(ranges[0])) {
+			_errors.push($('<span>'+_error_dictionary[3]+'</span>'+_common_text[1]+escapeHtml(val)+'</span>')); 
+			return;
+		}
+		var lessThanValue = val.slice(1,val.length);
+		if (isNumeric(lessThanValue) == true) { 
+			ranges[0] = lessThanValue;
+		} else {
+			_errors.push($('<span>'+_error_dictionary[0]+'</span>'+_common_text[1]+escapeHtml(val)+'</span>'));
+		}
+	}
+
+	if (val.startsWith('>')) {
+		if (!isNaN(ranges[1])) {
+			_errors.push($('<span>'+_error_dictionary[3]+'</span>'+_common_text[1]+escapeHtml(val)+'</span>'));
+			return;
+		}
+		var greaterThanValue = val.slice(1,val.length);
+		if (isNumeric(greaterThanValue) == true) { 
+			ranges[1] = greaterThanValue;
+		} else {
+			_errors.push($('<span>'+_error_dictionary[1]+'</span>'+_common_text[1]+escapeHtml(val)+'</span>'));
+		}
 	}
 }
